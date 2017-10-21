ee15c90141c1de479e514977ff9b20158ff183ec
arroheim <abigor.int3h@gmail.com>
2017-10-21 14:24:40 +0300

    Add files via upload

---
+++ src/main/messages.py
@@ -0,0 +1,6 @@
+import platform
+
+msg_welcome = 'Welcome My Bot \n Click here test - /test \n list bot - /list'
+msg_test = 'Yes, it is working properly \n goodbye :)'
+msg_list = '/start - start \n /help - help \n /test - test \n-'
+msg_os = 'Your os is:\n' + platform.system() +'\n'+ platform.processor()
